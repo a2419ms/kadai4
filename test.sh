b20b584e4cc66675c0ec95f727b8ca653ed64af5
a2419ms <a2419ms@aiit.ac.jp>
2024-06-08 16:28:00 +0000

    test.sh mod

--- test.sh
+++ test.sh
@@ -1,15 +1,16 @@
 #!/bin/bash
-#実行結果の整数を取得
+#実行結果を取得
 result=$(./kadai4.sh)
 
 #実行結果が整数値であるかを確認
 if [[ $result =~ ^[0-9]+$ ]]; then
 	echo "ok"
 	exit 0
-	#array=("ok") 後で実装できたらやります
+#実行時において、xよりyが大きくないかを確認
 elif [[ $result = "error 1" ]]; then
 	echo "x < y : ng"
 	exit 1
+#失敗時について、0以上の整数値であることを確認
 elif [[ $result = "error 2" ]]; then
 	echo "(x | y) is 0 > ( x | y )"
 	exit 2
